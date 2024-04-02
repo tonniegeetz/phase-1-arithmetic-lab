4490e305b2c9f87ac48e4f008d4db5841a699a67
AnthonyG-Dev <giterugicheru@gmail.com>
2024-04-03 01:32:42 +0300

    Understand various mathematics built-in methods

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random());
+
+let num3 = 29;
+let num4 = 5;
+const mod = 29 % 5;
+
+const max = Math.max(20, 4, 12, 3, 5, 4, 0);
